2447d4c04a2b2dbe21b4a7eea25275190d2d7091
alex <davidsoatse@.com>
2026-02-06 16:45:09 +0100

    arranger le backend et la page de connexion

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
-from django.urls import path,include
-from inventory.views import home
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    #path('', home, name='home'),
-    path('', include('inventory.urls'))
-# dire au projet d'aller regarder le dossier inventor pour le visuel
+    path('admin/', admin.site.urls),  # <--- C'est "urls" et non "id_index"
+    path('', include('inventory.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- inventory/urls.py
+++ inventory/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views # Ajoute cet import
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('voiture/<int:pk>/', views.car_detail, name='car_detail'),
-    path('login/', views.login_view, name='login'),
+    # Remplace ta ligne login par celle-ci :
+    path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name='login'),
+    # Ajoute aussi la déconnexion pour que le bouton fonctionne :
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
